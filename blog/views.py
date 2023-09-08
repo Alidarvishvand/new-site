0d5bba5755f9ef23727ee744f87c442aeda5d13d
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2023-09-08 18:37:59 +0330

    render
    Transferring data to the page using render

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ def blog_view(request):
   return render (request,'blog/blog-home.html')
 
 def blog_single(request):
-    
-  return render (request,'blog/blog-single.html')
+  context = {'title':'bitcoin crashed again','content':'bitcoin was flying but now', 'author':'alidrv'}  
+  return render (request,'blog/blog-single.html',context) 
--- website/views.py
+++ website/views.py
@@ -2,12 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 
-
-
-
 def index_view(request):
     return render(request, 'website/index.html')
-
 
 
 def about_view(request):
@@ -15,9 +11,11 @@ def about_view(request):
     return render(request, 'website/about.html')
 
 
-
-
 def contact_view(request):
     
     return render(request, 'website/contact.html')
 
+
+def test_view(request):
+
+    return render(request, 'website/test.html',{'name':'ali','lastname':'darvishvand'})
