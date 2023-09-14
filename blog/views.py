42b36a4c3431b211f53666edd97cd32a19fc1182
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2023-09-14 17:25:03 +0330

    posts.filter
    filter post  :
    +search form
    +author
    +

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 # Create your views here.
-def blog_view(request):
+def blog_view(request,cat_name=None,author_username=None):
   posts = Post.objects.filter(status=1)
+  if cat_name: 
+      posts = posts.filter(category_name=cat_name)
+  if author_username:
+      posts = posts.filter(author__username=author_username)
   context = {'posts': posts}
   return render (request,'blog/blog-home.html',context)
 
@@ -19,3 +23,20 @@ def test (request):
  # post = get_object_or_404(Post,pk=pid)
   #context = {'post':post}
   return render (request,'test.html')
+
+def blog_category(request,cat_name):
+  posts = Post.objects.filter(status=1)
+  posts = posts.filter(category__name=cat_name)
+  context  = {'posts':posts}
+  return render(request,'blog/blog-home.html',context)
+
+
+def blog_search(request):
+   posts = Post.objects.filter(status=1)
+   if request.method == 'GET':
+      if s := request.GET.get('s'):
+         posts = posts.filter(content__contains=request.GET.get('s'))
+
+
+   context = {'posts':posts}
+   return render(request,'blog/blog-home.html',context)
