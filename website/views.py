5932c2585c49cc802897346be14741e1bd7b1c61
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2023-09-17 18:53:54 +0330

    creat forms in pages
    in test.html we created a forms with fields...

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
-
+from website.models import Contact
+from website.forms import NameForm
 def index_view(request):
     return render(request, 'website/index.html')
 
@@ -17,5 +18,18 @@ def contact_view(request):
 
 
 def test_view(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            name= form.cleaned_data['name']
+            subject= form.cleaned_data['subject']
+            mail= form.cleaned_data['mail']
+            message= form.cleaned_data['message']
+            print(name,subject,mail,message)
+            return HttpResponse('done')
+        else:
+            return HttpResponse('not_valid')
 
-    return render(request, 'website/test.html',{'name':'ali','lastname':'darvishvand'})
+    form = NameForm()
+    return render(request,'test.html',{'form':form})
+ 
