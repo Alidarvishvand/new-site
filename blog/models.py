d3c85182d211432ad8605b7e5661765b28819dd9
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2023-09-19 17:22:38 +0330

    work with module
    +module humanize
    +module django extensions
    +module sites framework

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 class Category(models.Model):
     name=models.CharField( max_length=255)
@@ -29,5 +29,11 @@ class Post(models.Model):
 
     def snippets(self):
             return self.content[:100] + "..."
+    
+
+    def get_absolute_url(self):
+     return reverse('blog:single',kwargs={'pid':self.id})
+       
+    
 
 
