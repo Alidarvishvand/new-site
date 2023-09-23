7f38d3272a2a0017e0aaa0548fc6bc0359548be2
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2023-09-23 10:11:05 +0330

    disqus module & creat comment systeming
    blog-->admin.py&models.py
    
    disqus-->blogsingle

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Post,Category
 # Register your models here.
 from django_summernote.admin import SummernoteModelAdmin
-
+from blog.models import Comment
 class PostAdmin(SummernoteModelAdmin):
     date_hierarchy = 'created_date'
     #emoty_value_display = '-empty-'
@@ -13,5 +13,17 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
 
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+        #emoty_value_display = '-empty-'
+    list_display= ('name','post','approved','created_date')
+    list_filter = ('post','approved')
+    ordering = ['-created_date']
+    search_fields = ('name','post')
+    #summernote_fields = ('content',)
+
+
+
+admin.site.register(Comment,CommentAdmin)
 admin.site.register(Category)
 admin.site.register(Post,PostAdmin)
